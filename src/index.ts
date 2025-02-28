32cc388238e65fca6d5122a6a3821ad5af2d18f6
Vasiliy Zadoya <cool29@yandex.ru>
2025-03-01 01:32:19 +0500

    add cors middleware

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import todoController from './todoController';
+const cors = require('cors')
 
 const app = express();
+app.use(cors())
 
 app.use(bodyParser.json());
 
